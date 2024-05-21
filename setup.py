a51760891d75b112cd6c6aa63e8c3ad048415e86
markusuwe <markus.u.mueller@gmail.com>
2024-05-21 14:29:19 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 parent_dir = Path(__file__).resolve().parent
 
 setup(
-    name="fake-geo-images",
-    version="0.1.5",
-    author="UP42",
-    author_email="support@up42.com",
-    description="Fake geospatial images for unit tests",
+    name="geomockimages",
+    version="0.2.0",
+    author="Markus Mueller",
+    author_email="markus.u.mueller@zoho.com",
+    description="Geospatial mock images for unit tests",
     long_description=parent_dir.joinpath("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/up42/fake-geo-images",
@@ -18,7 +18,7 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
     ],
     install_requires=parent_dir.joinpath("requirements.txt")
